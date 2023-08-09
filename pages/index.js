e0fe21bf066e81d4ba42d119640031aed6e4dad9
Wsine <weizhy3@hotmail.com>
2023-08-09 13:34:05 +0800

    add: wrap item

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Container, VStack, Input, Button, Heading, ButtonGroup, Flex, Spacer } from '@chakra-ui/react'
+import { Container, VStack, Input, Button, Heading, ButtonGroup, Flex, Spacer, Wrap, WrapItem } from '@chakra-ui/react'
 import { useState } from 'react'
 
 export default function Index() {
@@ -10,45 +10,46 @@ export default function Index() {
         return {
           id: id,
           word: s.word,
-          color: s.color === 'teal' ? '' : 'teal'
+          selected: !s.selected
         }
       })
     })
   }
 
   return (
-    <Container maxW='3xl' mt={4} centerContent>
-      <VStack spacing={4} width='100%'>
+    <Container maxW='3xl' mt={4}>
+      <VStack spacing={4}>
         <Heading size='lg'>Lexicon Service</Heading>
         <Input
           placeholder='Context sentence'
-          width='100%'
           onChange={(e) => {
             const sentence = e.target.value.trim()
-            console.log(sentence)
             const words = sentence.split(' ').filter((word) => word.length > 0)
             setWordStates(words.map((word, index) => {
               return {
                 id: index,
                 word: word,
-                color: 'teal'
+                selected: false
               }
             }))
           }}
         />
-        <ButtonGroup size='sm' spacing={4} variant='outline'>
-          {wordStates.map((s) => {
-            return (
-              <Button
-                key={s.id}
-                colorScheme={s.color}
-                onClick={() => handleWordClick(s.id) }>
-                {s.word}
-              </Button>
-            )
-          })}
+        <ButtonGroup size='sm' spacing={4} colorScheme='teal'>
+          <Wrap>
+            {wordStates.map((s) => {
+              return (
+                <WrapItem>
+                  <Button
+                    variant={s.selected ? 'outline' : 'ghost'}
+                    onClick={() => handleWordClick(s.id) }>
+                    {s.word}
+                  </Button>
+                </WrapItem>
+              )
+            })}
+          </Wrap>
         </ButtonGroup>
-        <Flex width='100%'>
+        <Flex w='100%'>
           <Spacer />
           <Button colorScheme='teal' variant='solid' isLoading={false} loadingText='Submitting'>
             Add
