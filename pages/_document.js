7351388c4c9ca5f1b4c73e26753fc562c27aac56
Wsine <weizhy3@hotmail.com>
2023-08-10 13:45:35 +0800

    add: pwa settings

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="application-name" content="Lexicon Service" />
+        <meta name="description" content="Personal Lexicon Service" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#55acee" />
+
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
